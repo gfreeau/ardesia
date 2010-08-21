701914bc9d2aa9703241403cdc893151012fb35e
alpha@paranoici.org <alpha@paranoici.org@9b9de0c4-aa7d-11de-8c46-7bd654735883>
2010-08-21 13:57:29 +0000

    A first experimental version to record and forward the screencast with vlc in windows also

--- src/scripts/screencast.bat
+++ src/scripts/screencast.bat
@@ -8,16 +8,22 @@ rem if it is used a localhost icecast server with the default
 rem configuration
 rem Client side you must have the ezstream installed
 
-set ICECAST="FALSE";
-rem set ICECAST="TRUE";
+set ICECAST=FALSE
+rem set ICECAST=TRUE
+
+rem You must configure the right password; I put the default one
+set ICECAST_PASSWORD=hackme
+set ICECAST_ADDRESS=127.0.0.1
+set ICECAST_PORT=8000
+set ICECAST_MOUNTPOINT=ardesia.ogg
 
 rem This is the location of the vlc executable
 set RECORDER_LOCATION="C:\Program Files (x86)\VideoLAN\VLC"
 
+
 set RECORDER_PROGRAM=vlc.exe
 set RECORDER_PROGRAM_OPTIONS=-vvv -I dummy screen:// --screen-fps=12 --sout "#transcode{venc=theora,quality:10,scale=0.75,fps=12}:duplicate{dst=std{access=file,mux=ogg,dst=%2}}"
-set STREAMER_COMMAND=ezstream.exe
-set STREAMER_OPTIONS=-v -c ezstream_stdin_ardesia.xml 
+set RECORDER_AND_FORWARD_PROGRAM_OPTIONS=-vvv -I dummy screen:// --screen-fps=12 --sout "#transcode{venc=theora,quality:10,scale=0.75,fps=12}:duplicate{dst=std{access=shout,mux=ogg,dst=source:%ICECAST_PASSWORD%@%ICECAST_ADDRESS%:%ICECAST_PORT%/%ICECAST_MOUNTPOINT%},dst=std{access=file,mux=ogg,dst=%2}}"
 
 if ""%1"" == ""start"" goto start
 
@@ -25,32 +31,25 @@ if ""%1"" == ""stop"" goto stop
 
 :start
 rem This start the recording on file
+if "%ICECAST%" == "TRUE" goto icecast_start
+rem if not icecast then only record the screencast
 echo Start the screencast running %RECORDER_PROGRAM%
 echo With arguments %RECORDER_PROGRAM_OPTIONS%
 rem exec recorder
 start /B cmd /c "%RECORDER_LOCATION%\%RECORDER_PROGRAM% %RECORDER_PROGRAM_OPTIONS%"
-if %ICECAST% == "TRUE" goto icecast_start
 goto end
 
 :icecast_start
-rem wait some time
-ping -n 20 -w 1000 0.0.0.1 >NUL 2>&1
-rem start the stream to icecast with ezstream
-echo Forwarding the streaming %2 to icecast server 
-start /B cmd /c "type %2 | %STREAMER_COMMAND% %STREAMER_OPTIONS%"
+echo Start the screencast running %RECORDER_PROGRAM%
+echo With arguments %RECORDER_AND_FORWARD_PROGRAM_OPTIONS%
+rem exec recorder
+start /B cmd /c "%RECORDER_LOCATION%\%RECORDER_PROGRAM% %RECORDER_AND_FORWARD_PROGRAM_OPTIONS%"
 goto end
 
 :stop
 set RECORDER_PID=cat %RECORDER_PID_FILE%
 echo Stop the screencast killing %RECORDER_PROGRAM% 
 TASKKILL /F /IM %RECORDER_PROGRAM%
-if %ICECAST% == ""TRUE"" goto icecast_stop
-goto end
-
-:icecast_stop
-echo Stopping the streaming to icecast server
-rem stop the stream to icecast killing ezstream
-TASKKILL /F /IM %STREAMER_COMMAND%
 goto end
 
 :end
