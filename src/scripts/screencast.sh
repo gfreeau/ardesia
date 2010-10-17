b126a51385491c7494aab9fe9ed94157428d719a
alpha@paranoici.org <alpha@paranoici.org@9b9de0c4-aa7d-11de-8c46-7bd654735883>
2010-10-17 11:55:16 +0000

    Add audio sync and interlace option in  recording scripts REMARKS: In my computer the audio and video are not yet synchronized, I haven'idea if it is a vlc bug or the cpu power in not enought... I'm waiting for feedback

--- src/scripts/screencast.sh
+++ src/scripts/screencast.sh
@@ -27,9 +27,9 @@ then
   echo Start the screencast running $RECORDER_PROGRAM
   if [ "$ICECAST" = "TRUE" ]
   then
-    RECORDER_PROGRAM_OPTIONS="-vvv screen:// --screen-fps=12 :input-slave=alsa:// --sout  "#transcode{vcodec=theo,vb=512,scale=0.75,acodec=vorb,ab=128,channels=2,samplerate=32000}:duplicate{dst=std{access=shout,mux=ogg,dst=source:$ICECAST_PASSWORD@$ICECAST_ADDRESS:$ICECAST_PORT/$ICECAST_MOUNTPOINT},dst=std{access=file,mux=ogg,dst=$2}}"" 
+    RECORDER_PROGRAM_OPTIONS="-vvv screen:// --screen-fps=12 :input-slave=alsa:// --sout  "#transcode{vcodec=theo,vb=512,scale=0.75,acodec=vorb,ab=128,channels=2,,deinterlace,audio-sync,samplerate=32000}:duplicate{dst=std{access=shout,mux=ogg,dst=source:$ICECAST_PASSWORD@$ICECAST_ADDRESS:$ICECAST_PORT/$ICECAST_MOUNTPOINT},dst=std{access=file,mux=ogg,dst=$2}}"" 
   else
-    RECORDER_PROGRAM_OPTIONS="-vvv screen:// --screen-fps=12 :input-slave=alsa:// --sout "#transcode{vcodec=theo,vb=512,scale=0.75,acodec=vorb,ab=128,channels=2,samplerate=32000}:duplicate{dst=std{access=file,mux=ogg,dst=$2}}"" 
+    RECORDER_PROGRAM_OPTIONS="-vvv screen:// --screen-fps=12 :input-slave=alsa:// --sout "#transcode{vcodec=theo,fps=12,vb=512,scale=0.75,acodec=vorb,ab=64,channels=2,deinterlace,audio-sync,samplerate=32000}:duplicate{dst=std{access=file,mux=ogg,dst=$2}}"" 
   fi
   echo With arguments $RECORDER_PROGRAM_OPTIONS
   $RECORDER_PROGRAM $RECORDER_PROGRAM_OPTIONS &
