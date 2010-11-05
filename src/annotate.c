8b9a5d2c1c32c95ba1f1925d9be7769cac2a9563
alpha@paranoici.org <alpha@paranoici.org@9b9de0c4-aa7d-11de-8c46-7bd654735883>
2010-11-05 18:04:53 +0000

    Fix of previous commit

--- src/annotate.c
+++ src/annotate.c
@@ -1441,7 +1441,7 @@ void setup_app(GtkWidget* parent)
   gtk_widget_set_double_buffered(data->annotation_window, FALSE); 
   /* @TODO use RGBA colormap and avoid to use the layered window */
   /* I use a layered window that use the black as transparent color */
-  setLayeredGdkWindowAttributes(data->annotation_window->window, RGBA(0,0,0), 0, LWA_COLORKEY );	
+  setLayeredGdkWindowAttributes(data->annotation_window->window, RGB(0,0,0), 0, LWA_COLORKEY );	
 #endif
 }
 
