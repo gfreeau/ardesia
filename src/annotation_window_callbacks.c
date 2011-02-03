3e700fc2921f0af0d30b226c497a96849be61d60
alpha@paranoici.org <alpha@paranoici.org@9b9de0c4-aa7d-11de-8c46-7bd654735883>
2011-02-03 15:41:10 +0000

    Adjustment for linux

--- src/annotation_window_callbacks.c
+++ src/annotation_window_callbacks.c
@@ -92,7 +92,10 @@ event_expose(GtkWidget *widget,
       if (data->savelist == NULL)
         {
 	  annotate_clear_screen();
-        } 		      
+        }
+#ifndef _WIN32
+      gtk_window_set_opacity(GTK_WINDOW(data->annotation_window), 1);
+#endif 		      
     }
   /* data->annotation_cairo_context is not NULL */
   annotate_restore_surface();
