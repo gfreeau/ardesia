2eace56b6a85818a40a6dad465042658ce44d532
pilolli.pietro@gmail.com <pilolli.pietro@gmail.com@9b9de0c4-aa7d-11de-8c46-7bd654735883>
2013-05-02 12:30:25 +0000

    Adjustment in theme

--- src/bar.c
+++ src/bar.c
@@ -213,7 +213,8 @@ create_bar_window (CommandLine *commandline,
 
   bar_data = init_bar_data ();
 
-  bar_window = GTK_WIDGET (gtk_builder_get_object (bar_gtk_builder, "winMain"));
+  bar_window = GTK_WIDGET (gtk_builder_get_object (bar_gtk_builder, "ArdesiaBar"));
+  gtk_widget_set_name (bar_window, "ArdesiaBar");
     
   /* Connect all the callback from bar_gtk_builder xml file. */
   gtk_builder_connect_signals (bar_gtk_builder, (gpointer) bar_data);
